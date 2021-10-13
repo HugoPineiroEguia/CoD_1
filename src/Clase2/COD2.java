ddc2b9dbc25ec7527fd67ff99e0add12bf9559b6
Hugo Pineiro Eguia <hpineiroeguia@danielcastelao.org>
2021-10-13 09:09:42 +0200

    Me gusta la reguetona

---
+++ src/Clase2/COD2.java
@@ -0,0 +1,10 @@
+package Clase2;
+
+public class COD2 {
+
+    public static void Main (String[] args) {
+
+        System.out.println("Esta es la clase 2, y ahora me gusta la reguetona");
+
+    }
+}
